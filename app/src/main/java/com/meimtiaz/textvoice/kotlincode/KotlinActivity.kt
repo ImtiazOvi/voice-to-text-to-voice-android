c35e20d7787f224ad597e673b38780bb659bf795
imtiaz <imtiazuddin018@gmail.com>
2022-09-14 15:30:31 +0600

    Text to voice impl

--- app/src/main/java/com/meimtiaz/textvoice/kotlincode/KotlinActivity.kt
+++ app/src/main/java/com/meimtiaz/textvoice/kotlincode/KotlinActivity.kt
@@ -4,7 +4,9 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
@@ -12,20 +14,44 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.meimtiaz.textvoice.R
 
-class KotlinActivity : AppCompatActivity() {
+class KotlinActivity : AppCompatActivity(){
     private var voiceResultTv: TextView? = null
+    private lateinit var textToSpeech : TextToSpeech
+    private lateinit var textEt : EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         voiceResultTv = findViewById(R.id.voiceResultTv)
+        textEt = findViewById(R.id.textEt)
         val takeVoiceBtn = findViewById<TextView>(R.id.takeVoiceBtn)
+        val listenVoiceBtn = findViewById<TextView>(R.id.listenVoiceBtn)
 
+        textToSpeech = TextToSpeech(this) {}
+
+        // voice to text
         takeVoiceBtn.setOnClickListener { view: View? ->
             getSpeechInput(
                 view
             )
         }
+
+        // text to voice
+        listenVoiceBtn.setOnClickListener {
+            textToSpeech.stop()
+            textToSpeech.speak( textEt.text.toString(), TextToSpeech.QUEUE_FLUSH, null, "")
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        textToSpeech.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        textToSpeech.stop()
     }
 
     private fun getSpeechInput(view: View?) {
